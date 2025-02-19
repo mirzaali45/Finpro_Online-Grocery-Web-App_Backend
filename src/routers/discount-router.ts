3da63e24dd21e41a0c9e54b6c552f970e3607c7d
crayoninvok <dathariqf@gmail.com>
2025-02-20 00:26:06 +0700

    discount fix

--- src/routers/discount-router.ts
+++ src/routers/discount-router.ts
@@ -3,46 +3,56 @@ import { DiscountController } from "../controllers/discount-controller";
 import { AuthMiddleware } from "../middleware/auth.verify";
 
 export class DiscountRouter {
-    private router: Router;
-    private discountController: DiscountController;
-    private authMiddleware: AuthMiddleware;
-  
-    constructor() {
-      this.router = Router();
-      this.discountController = new DiscountController();
-      this.authMiddleware = new AuthMiddleware();
-      this.initializeRoutes();
-    }
-  
-    private initializeRoutes() {
-      this.router.get("/", this.discountController.getAllDiscounts);
-      this.router.get("/active", this.discountController.getActiveDiscounts);
-      this.router.get("/store/:storeId", this.discountController.getStoreDiscounts);
-      this.router.get("/:id", this.discountController.getDiscountById);
-      
-      this.router.post(
-        "/create",
-        this.authMiddleware.verifyToken,
-        this.authMiddleware.checkStrAdmin,
-        this.discountController.createDiscount
-      );
-      
-      this.router.put(
-        "/:id",
-        this.authMiddleware.verifyToken,
-        this.authMiddleware.checkStrAdmin,
-        this.discountController.updateDiscount
-      );
-      
-      this.router.delete(
-        "/:id",
-        this.authMiddleware.verifyToken,
-        this.authMiddleware.checkStrAdmin,
-        this.discountController.deleteDiscount
-      );
-    }
-  
-    getRouter(): Router {
-      return this.router;
-    }
+  private router: Router;
+  private discountController: DiscountController;
+  private authMiddleware: AuthMiddleware;
+
+  constructor() {
+    this.router = Router();
+    this.discountController = new DiscountController();
+    this.authMiddleware = new AuthMiddleware();
+    this.initializeRoutes();
   }
+
+  private initializeRoutes() {
+    this.router.get("/", this.discountController.getAllDiscounts);
+    this.router.get(
+      "/getproduct",
+      this.authMiddleware.checkStrAdmin,
+      this.discountController.getProductStore
+    );
+    this.router.get("/active", this.discountController.getActiveDiscounts);
+    this.router.get(
+      "/store/:storeId",
+      this.discountController.getStoreDiscounts
+    );
+    this.router.get("/:id", this.discountController.getDiscountById);
+
+    this.router.post(
+      "/create",
+      this.authMiddleware.verifyToken,
+      this.authMiddleware.checkStrAdmin,
+      this.discountController.createDiscount
+    );
+
+    this.router.put(
+      "/:id",
+      this.authMiddleware.verifyToken,
+      this.authMiddleware.checkStrAdmin,
+      this.discountController.updateDiscount
+    );
+
+    this.router.delete(
+      "/:id",
+      this.authMiddleware.verifyToken,
+      this.authMiddleware.checkStrAdmin,
+      this.discountController.deleteDiscount
+    );
+    
+ 
+  }
+
+  getRouter(): Router {
+    return this.router;
+  }
+}
