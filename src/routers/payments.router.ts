5e7390b640a55a879c4708975f0f24ac5b138f20
mirzaali45 <mirzaaliyusuf45@gmail.com>
2025-02-28 17:04:31 +0700

    update

--- src/routers/payments.router.ts
+++ src/routers/payments.router.ts
@@ -1,20 +1,20 @@
 import { Router } from "express";
-// import { AuthMiddleware } from "../middleware/auth.verify";
+import { AuthMiddleware } from "../middleware/auth.verify";
 import { PaymentsController } from "../controllers/payments.controller";
 
 export class PaymentsRouter {
   private router: Router;
   private paymentsController: PaymentsController;
-  // private authMiddleware: AuthMiddleware;
+  private authMiddleware: AuthMiddleware;
 
   constructor() {
     this.router = Router();
     this.paymentsController = new PaymentsController();
-    // this.authMiddleware = new AuthMiddleware();
+    this.authMiddleware = new AuthMiddleware();
     this.initializeRoutes();
   }
   private initializeRoutes() {
-    this.router.get(
+    this.router.post(
       "/snap-token",
       this.paymentsController.createSnapToken
     );
