d32c52a62b9a751c0267962707110240cbd960cc
mirzaali45 <mirzaaliyusuf45@gmail.com>
2025-03-07 13:52:48 +0700

    pull main

--- src/helpers/orderCron.ts
+++
@@ -1,9 +0,0 @@
-// cronJobs/orderCron.ts
-import cron from 'node-cron';
-import { autoConfirmOrder } from '../controllers/orderController';
-
-// Menjadwalkan pekerjaan otomatis setiap jam
-cron.schedule('0 * * * *', () => {
-  console.log('Running auto-confirm job...');
-  autoConfirmOrder();
-});
